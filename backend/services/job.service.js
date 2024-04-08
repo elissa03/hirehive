d4ae63d24dd6152081bc2b71f554975b48bcf4e7
Jana Aridi <122409645+janaaridi@users.noreply.github.com>
2024-04-09 00:34:42 +0300

    added doc for job create mising part

--- backend/services/job.service.js
+++ backend/services/job.service.js
@@ -6,7 +6,8 @@ import { isValidDate } from "./utils/utils.js";
  *
  * This request expects the following data upon creating a job offer:
  * 'userId': to add new Job to user model & postedBy in Job,
- * 'title', 'description', 'requirements': [], 'deadline' (optional), 'isCoverLetterNeeded' (optional): Job details 
+ * 'title', 'description', 'requirements': [], 'type': ('Part-time', 'Full-time', 'Internship', 'Project'),
+ * 'deadline' (optional), 'isCoverLetterNeeded' (optional): Job details 
  * 
  * @param {*} data: req body 
  * @returns : status and message of creation
